d91dbf3cbaf63e35d61d707441953d6f69fca17a
Fedor Anikeev <fedos3d@gmail.com>
2022-02-12 11:02:23 +0000

    added static content

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   var port = process.env.PORT || 3000
-  const app = await NestFactory.create(AppModule);
+  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  
   await app.listen(port);
 }
 bootstrap();
