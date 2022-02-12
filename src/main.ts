a9d2f586b372971e3d60ff7f3aa9fe9426fd9f7d
Fedor Anikeev <fedos3d@gmail.com>
2022-02-12 11:21:03 +0000

    added smth

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
+
   var port = process.env.PORT || 3000
   
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
