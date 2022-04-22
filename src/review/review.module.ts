35925b2c13a44c42434a066fef526a6dd49992e6
Fedor Anikeev <fedos3d@gmail.com>
2022-04-22 01:28:59 +0000

    minor fixes

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,6 +3,7 @@ import { ReviewController } from "./review.controller";
 import { ReviewService } from "./review.service";
 import { PrismaService } from "../prisma.service"
 
+
 @Module({
   controllers: [ReviewController],
   providers: [ReviewService, PrismaService]
