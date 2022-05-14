df510287a9fac3efd6918a8c89298667e2d984a9
Fedor Anikeev <fedos3d@gmail.com>
2022-05-14 11:30:08 +0000

    Fixed DB diagram, added new pic :)

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication  } from '@nestjs/platform-express';
-import { join } from 'path';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 import { PrismaService } from "./prisma.service";
-import * as session from 'express-session';
 
 async function bootstrap() {
-
   var port = process.env.PORT || 3000;
-  
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
- 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  
+
+  app.useStaticAssets(join(__dirname, "..", "public"));
+
   //Two lines below are responsible for pug template
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('pug');
+  app.setBaseViewsDir(join(__dirname, "..", "views"));
+  app.setViewEngine("pug");
 
   await app.listen(port);
 
   const prismaService = app.get<PrismaService>(PrismaService);
-  await prismaService.enableShutdownHooks(app)
-
+  await prismaService.enableShutdownHooks(app);
 }
 bootstrap();
