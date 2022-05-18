6b8b20463e8842e8e3aef5af238a2648d17cf703
Fedor Anikeev <fedos3d@gmail.com>
2022-05-18 14:04:20 +0000

    Minor fixes

--- src/filters/Forbbiden.filter.ts
+++ src/filters/Forbbiden.filter.ts
@@ -3,21 +3,22 @@ import {
   Catch,
   ArgumentsHost,
   NotFoundException,
-  ForbiddenException
-} from '@nestjs/common'
-import { Request, Response } from 'express'
+  ForbiddenException,
+} from "@nestjs/common";
+import { Request, Response } from "express";
 
 @Catch(ForbiddenException)
 export class ForbiddenExceptionFilter implements ExceptionFilter {
-  catch (exception: ForbiddenException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp()
-    const response = ctx.getResponse<Response>()
-    const request = ctx.getRequest<Request>()
-    const status = exception.getStatus()
+  catch(exception: ForbiddenException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
 
-    return response.status(status).render('error.pug', {
+    return response.status(status).render("error.pug", {
       description: exception.message,
-      status
-    })
+      status,
+      ref: request.get("Referrer"),
+    });
   }
 }
--- src/filters/NotFound.filter.ts
+++ src/filters/NotFound.filter.ts
@@ -2,21 +2,22 @@ import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
-  NotFoundException
-} from '@nestjs/common'
-import { Request, Response } from 'express'
+  NotFoundException,
+} from "@nestjs/common";
+import { Request, Response } from "express";
 
 @Catch(NotFoundException)
 export class NotFoundExceptionFilter implements ExceptionFilter {
-  catch (exception: NotFoundException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp()
-    const response = ctx.getResponse<Response>()
-    const request = ctx.getRequest<Request>()
-    const status = exception.getStatus()
+  catch(exception: NotFoundException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
 
-    return response.status(status).render('error.pug', {
+    return response.status(status).render("error.pug", {
       description: exception.message,
-      status
-    })
+      status,
+      ref: request.get("Referrer"),
+    });
   }
 }
